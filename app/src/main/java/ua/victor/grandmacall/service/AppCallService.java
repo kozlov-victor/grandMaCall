65f53e1aad5b46a70c8b910ccae5057d61a60564
Kozlov Victor <kozlovvictorzx@gmail.com>
2021-11-11 23:45:26 +0200

    initial commit

--- app/src/main/java/ua/victor/grandmacall/service/AppCallService.java
+++ app/src/main/java/ua/victor/grandmacall/service/AppCallService.java
@@ -10,6 +10,9 @@ import android.telecom.InCallService;
 
 import androidx.annotation.RequiresApi;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 import ua.victor.grandmacall.MainActivity;
 
 @RequiresApi(api = Build.VERSION_CODES.M)
@@ -20,55 +23,22 @@ public class AppCallService extends InCallService {
     public static final String EXTRA_STATE = "app.extra.state";
     public static final String EXTRA_NUMBER = "app.extra.number";
 
-    public AppCallService() {
-        System.out.println("--------------------ctor-------------------------");
-    }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        System.out.println("-----------------------bind-------------------------" + intent);
-        return super.onBind(intent);
-    }
-
-    @Override
-    public void onConnectionEvent(Call call, String event, Bundle extras) {
-        System.out.println("-------------------on connection event--- " + event);
-        super.onConnectionEvent(call, event, extras);
-    }
-
-    @Override
-    public void onCallAudioStateChanged(CallAudioState audioState) {
-        System.out.println("-------------------onCallAudioStateChanged--------------- " + audioState);
-        super.onCallAudioStateChanged(audioState);
-    }
-
-    @Override
-    public boolean onUnbind(Intent intent) {
-        System.out.println("-------------------onUnbind--------------- " + intent);
-        return super.onUnbind(intent);
-    }
-
-    @Override
-    public void onBringToForeground(boolean showDialpad) {
-        System.out.println("-------------------onBringToForeground--------------- " + showDialpad);
-        super.onBringToForeground(showDialpad);
-    }
 
     @Override
     public void onCallAdded(Call call) {
-        System.out.println("--------------onCallAdded -------------" + call);
         super.onCallAdded(call);
         currentCall = call;
         Intent intent = new Intent();
         intent.setClass(getApplicationContext(),MainActivity.class);
+        String phone = call.getDetails().getHandle().getSchemeSpecificPart();
         intent.putExtra(EXTRA_STATE,call.getState());
-        intent.putExtra(EXTRA_NUMBER,call.getDetails().getHandle().getSchemeSpecificPart());
+        intent.putExtra(EXTRA_NUMBER,phone);
         getApplicationContext().startActivity(intent);
     }
 
     @Override
     public void onCallRemoved(Call call) {
-        System.out.println("--------------onCallRemoved-------------" + call);
         super.onCallRemoved(call);
         if (MainActivity.getInstance()!=null) MainActivity.getInstance().finish();
     }
